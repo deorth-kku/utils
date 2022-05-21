574ce103c305e301a99a4a7b299d2807c9d1c4fa
deorth-boffin <deorth@outlook.com>
2022-05-21 23:20:51 +0800

    add support for build-in lib ZipFile

--- _Decompress.py
+++ _Decompress.py
@@ -3,29 +3,40 @@ from typing import Generator
 import logging
 import os
 from importlib import reload, import_module
+from zipfile import ZipFile
 
 
 class Decompress():
     # Still 2 things needs to be done:
-    # 1. If libarchive not installed use building zip library
+    # 1. If libarchive not installed use build-in zip library
     # 2. If input file is and exe, try to seperate 7z file from PE part.
-    libarchive = None
+    try:
+        libarchive = import_module("libarchive")
+    except (ImportError, TypeError, OSError):
+        libarchive = None
 
     @classmethod
-    def setLibarchive(cls, lib_file: str = None) -> None:
-        if lib_file:
-            os.environ.update({"LIBARCHIVE": lib_file})
-
+    def setLibarchive(cls, lib_file: str) -> None:
+        os.environ.update({"LIBARCHIVE": lib_file})
         if cls.libarchive:
             reload(cls.libarchive)
         else:
             cls.libarchive = import_module("libarchive")
 
-    def __init__(self, filename: os.PathLike) -> None:
-        if not self.libarchive:
-            self.libarchive = import_module("libarchive")
+    def __init__(self, filename: os.PathLike, use_zipfile=False) -> None:
         self.filename = filename
-        self.filelist = list(self.getFileList())
+        if not self.libarchive or use_zipfile:
+            logging.warning(
+                "using build-in ZipFile library on %s because libarchive is not available" % filename)
+            self.zf = ZipFile(self.filename)
+            self.getFileList = self.zf.namelist
+            self.extractAll = self.zf.extractall
+
+            def extractFiles(filenames: list, outdir: os.PathLike):
+                for file in filenames:
+                    self.zf.extract(file, outdir)
+            self.extractFiles = extractFiles
+            self.__del__=self.zf.close
 
     def getFileList(self) -> Generator:
         with self.libarchive.file_reader(self.filename) as archive:
@@ -33,6 +44,7 @@ class Decompress():
                 yield str(entry)
 
     def getPrefixDir(self) -> str:
+        self.filelist = list(self.getFileList())
         if len(self.filelist) == 1:
             dir = ""
         else:
@@ -44,11 +56,11 @@ class Decompress():
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         os.chdir(outdir)
-        entries = self.select_entries_with_names(filenames)
+        entries = self.__select_entries_with_names(filenames)
         self.libarchive.extract.extract_entries(entries)
         os.chdir(pwd_temp)
 
-    def select_entries_with_names(self, filenames: list) -> Generator:
+    def __select_entries_with_names(self, filenames: list) -> Generator:
         with self.libarchive.file_reader(self.filename) as archive:
             for entry in archive:
                 if entry.name in filenames:
@@ -64,18 +76,24 @@ class Decompress():
         os.chdir(pwd_temp)
 
 
+
 if __name__ == "__main__":
     from _Log import my_log_settings
     my_log_settings()
     from _Aria2Rpc import Aria2Rpc
     a = Aria2Rpc(port=6801, passwd="abc", rpc_listen_all=True,
                  rpc_allow_origin_all=True, all_proxy="http://127.0.0.1:8123")
-    temp_dir = "/mnt/temp"
+    temp_dir = "/mnt/temp/decompress"
+    os.makedirs(temp_dir,exist_ok=True)
     dl_urls = [
-        "https://github.com/dezem/SAK/releases/download/0.7.14/SAK_v0.7.14_64bit_20220405-19-38-49.7z",
-        "https://github.com/PDModdingCommunity/PD-Loader/releases/download/2.6.5a-r4n/PD-Loader-2.6.5a-r4.zip",
-        "https://somesite/7z2107-x64.exe"
+        # "https://github.com/dezem/SAK/releases/download/0.7.14/SAK_v0.7.14_64bit_20220405-19-38-49.7z",
+        #"https://github.com/PDModdingCommunity/PD-Loader/releases/download/2.6.5a-r4n/PD-Loader-2.6.5a-r4.zip",
+        "http://trash.com/sukidesuost_Kagurairo Artifact F.zip"
+        #"https://somesite/7z2107-x64.exe"
     ]
+    import time
+    from _Py7z import Py7z
+    import shutil
     for dl_url in dl_urls:
         download_file = os.path.join(temp_dir, os.path.basename(dl_url))
 
@@ -83,8 +101,36 @@ if __name__ == "__main__":
             a.wget(dl_url, dir=temp_dir)
 
         try:
+            out_dir="mafumafu - Kagurairo Artifact [FLAC]"
+            out_dir=os.path.join(temp_dir,out_dir)
+
+            shutil.rmtree(out_dir,ignore_errors=True)
+            starttime=time.time()
             f = Decompress(download_file)
-            print(f.getPrefixDir())
+            f.extractAll(temp_dir)
+            time_used=time.time()-starttime
+            logging.info("libarchive: %s"%time_used)
+
+            shutil.rmtree(out_dir,ignore_errors=True)
+            starttime=time.time()
+            f = Py7z(download_file)
+            f.extractAll(temp_dir)
+            time_used=time.time()-starttime
+            logging.info("7z: %s"%time_used)
+
+
+            shutil.rmtree(out_dir,ignore_errors=True)
+            starttime=time.time()
+            f = Decompress(download_file, use_zipfile=True)
+            f.extractAll(temp_dir)
+            time_used=time.time()-starttime
+            logging.info("zipfile: %s"%time_used)
+
+
+            
+
+
+            
         except Exception as e:
             logging.exception(e)
 
