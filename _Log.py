4efa0d2808faa313892ce38fe1271ce2e51e4dd8
deorth-boffin <deorth@outlook.com>
2022-05-24 17:09:53 +0800

    improve compatibility

--- _Log.py
+++ _Log.py
@@ -1,14 +1,22 @@
 #!/bin/python3
 import logging
+from platform import python_version_tuple
 
 
 def my_log_settings(log_file: str = None, log_level: str = "DEBUG") -> None:
     LOG_FORMAT = "%(asctime)s.%(msecs)03d [%(levelname)s] %(pathname)s:%(lineno)s | %(message)s "
     DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
     log_level = eval("logging.%s" % log_level.upper())
-    logging.basicConfig(level=log_level,
-                        format=LOG_FORMAT,
-                        datefmt=DATE_FORMAT,
-                        filename=log_file,
-                        force=True
-                        )
+    kwargs = {
+        "level": log_level,
+        "format": LOG_FORMAT,
+        "datefmt": DATE_FORMAT,
+        "filename": log_file
+    }
+    if int(python_version_tuple()[1]) > 7:
+        kwargs.update({"force": True})
+    logging.basicConfig(**kwargs)
+
+
+if __name__=="__main__":
+    pass
