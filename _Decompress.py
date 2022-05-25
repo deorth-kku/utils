c6fea4d63b511d4bc234a67dd478d9e9eadbc81c
deorth-boffin <deorth@outlook.com>
2022-05-25 11:05:04 +0800

    improve 7z sfx support

--- _Decompress.py
+++ _Decompress.py
@@ -4,6 +4,7 @@ import logging
 import os
 from importlib import reload, import_module
 from zipfile import ZipFile
+import pefile
 
 
 class Decompress():
@@ -23,23 +24,47 @@ class Decompress():
         else:
             cls.libarchive = import_module("libarchive")
 
-    def __init__(self, filename: os.PathLike, use_zipfile=False) -> None:
+    def __init__(self, filename: str, use_zipfile=False) -> None:
         self.filename = filename
         if not self.libarchive or use_zipfile:
             logging.warning(
-                "using build-in ZipFile library on %s because libarchive is not available" % filename)
+                "using build-in ZipFile library on %s because libarchive is not available or user forcing" % filename)
             self.zf = ZipFile(self.filename)
             self.getFileList = self.zf.namelist
             self.extractAll = self.zf.extractall
 
-            def extractFiles(filenames: list, outdir: os.PathLike):
+            def extractFiles(filenames: list, outdir: str):
                 for file in filenames:
                     self.zf.extract(file, outdir)
             self.extractFiles = extractFiles
             self.__del__ = self.zf.close
+        elif filename.endswith(".exe"):
+            logging.info("input an exe file, try extracting 7z sfx")
+            self.reader = self.libarchive.memory_reader
+            self.extracter = self.libarchive.extract_memory
+
+            pe = pefile.PE(filename)
+            last_section = pe.sections[-1]
+            pe_end = last_section.PointerToRawData+last_section.SizeOfRawData
+            logging.debug(
+                "pe file ends at %x, assume data after is 7z" % pe_end)
+            f = open(filename, "rb")
+            f.seek(pe_end)
+            while True:
+                if f.read(4) == b'\x37\x7a\xbc\xaf':
+                    startof7z = f.tell()-4
+                    logging.debug("found 7z header at 0x%x" % startof7z)
+                    break
+            f.seek(-4, 1)
+            self.read_from = f.read()
+            self.read_from = f.read()
+        else:
+            self.reader = self.libarchive.file_reader
+            self.extracter = self.libarchive.extract_file
+            self.read_from = filename
 
     def getFileList(self) -> Generator:
-        with self.libarchive.file_reader(self.filename) as archive:
+        with self.reader(self.read_from) as archive:
             for entry in archive:
                 if type(entry.name) == bytes:
                     logging.warning(
@@ -55,7 +80,7 @@ class Decompress():
             dir = os.path.commonpath(self.filelist)
         return dir
 
-    def extractFiles(self, filenames: list, outdir: os.PathLike) -> None:
+    def extractFiles(self, filenames: list, outdir: str) -> None:
         pwd_temp = os.getcwd()
         if not os.path.exists(outdir):
             os.makedirs(outdir)
@@ -65,18 +90,18 @@ class Decompress():
         os.chdir(pwd_temp)
 
     def __select_entries_with_names(self, filenames: list) -> Generator:
-        with self.libarchive.file_reader(self.filename) as archive:
+        with self.reader(self.read_from) as archive:
             for entry in archive:
                 if entry.name in filenames:
                     logging.debug("selected file '%s'" % entry.name)
                     yield entry
 
-    def extractAll(self, outdir: os.PathLike) -> None:
+    def extractAll(self, outdir: str) -> None:
         pwd_temp = os.getcwd()
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         os.chdir(outdir)
-        self.libarchive.extract_file(self.filename)
+        self.extracter(self.read_from)
         os.chdir(pwd_temp)
 
 
@@ -85,50 +110,30 @@ if __name__ == "__main__":
     my_log_settings()
     from _Aria2Rpc import Aria2Rpc
     a = Aria2Rpc(port=6801, passwd="abc", rpc_listen_all=True,
-                 rpc_allow_origin_all=True, all_proxy="http://127.0.0.1:8123")
+                 rpc_allow_origin_all=True, split=16, max_connection_per_server=16, all_proxy="http://127.0.0.1:8123")
     temp_dir = "/mnt/temp/decompress"
     os.makedirs(temp_dir, exist_ok=True)
     dl_urls = [
-        # "https://github.com/dezem/SAK/releases/download/0.7.14/SAK_v0.7.14_64bit_20220405-19-38-49.7z",
-        # "https://github.com/PDModdingCommunity/PD-Loader/releases/download/2.6.5a-r4n/PD-Loader-2.6.5a-r4.zip",
-        "http://trash.com/sukidesuost_Kagurairo Artifact F.zip"
-        # "https://somesite/7z2107-x64.exe"
+        "https://github.com/dezem/SAK/releases/download/0.7.14/SAK_v0.7.14_64bit_20220405-19-38-49.7z",
+        "https://github.com/PDModdingCommunity/PD-Loader/releases/download/2.6.5a-r4n/PD-Loader-2.6.5a-r4.zip",
+        "https://www.7-zip.org/a/7z2107-x64.exe"
     ]
     import time
-    from _Py7z import Py7z
     import shutil
     for dl_url in dl_urls:
         download_file = os.path.join(temp_dir, os.path.basename(dl_url))
 
-        if not os.path.exists(download_file):
-            a.wget(dl_url, dir=temp_dir)
+        if not os.path.exists(download_file) or os.path.exists(download_file+".aria2"):
+            a.wget(dl_url, dir=temp_dir, retry=50)
 
-        try:
-            out_dir = "mafumafu - Kagurairo Artifact [FLAC]"
-            out_dir = os.path.join(temp_dir, out_dir)
+        out_dir = os.path.basename(dl_url)+"_dir"
+        out_dir = os.path.join(temp_dir, out_dir)
 
-            shutil.rmtree(out_dir, ignore_errors=True)
-            starttime = time.time()
-            f = Decompress(download_file)
-            f.extractAll(temp_dir)
-            time_used = time.time()-starttime
-            logging.info("libarchive: %s" % time_used)
-
-            shutil.rmtree(out_dir, ignore_errors=True)
-            starttime = time.time()
-            f = Py7z(download_file)
-            f.extractAll(temp_dir)
-            time_used = time.time()-starttime
-            logging.info("7z: %s" % time_used)
-
-            shutil.rmtree(out_dir, ignore_errors=True)
-            starttime = time.time()
-            f = Decompress(download_file, use_zipfile=True)
-            f.extractAll(temp_dir)
-            time_used = time.time()-starttime
-            logging.info("zipfile: %s" % time_used)
-
-        except Exception as e:
-            logging.exception(e)
+        shutil.rmtree(out_dir, ignore_errors=True)
+        starttime = time.time()
+        f = Decompress(download_file)
+        f.extractAll(out_dir)
+        time_used = time.time()-starttime
+        logging.info("libarchive: %s" % time_used)
 
     a.quit()
