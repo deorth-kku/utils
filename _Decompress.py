4d57c8b804e9e6740af6a8f0642477ac06e56d41
deorth <deorth@outlook.com>
2022-07-26 16:46:31 +0800

    fix for libarchive(utils side)

--- _Decompress.py
+++ _Decompress.py
@@ -6,25 +6,15 @@ from importlib import reload, import_module
 from zipfile import ZipFile
 
 class Decompress():
-    # Still 2 things needs to be done:
-    # 1. If libarchive not installed use build-in zip library
-    # 2. If input file is and exe, try to seperate 7z file from PE part.
-    try:
-        libarchive = import_module("libarchive")
-    except (ImportError, TypeError, OSError):
-        libarchive = None
+    libarchive = None
 
     @classmethod
     def setLibarchive(cls, lib_file: str) -> None:
         os.environ.update({"LIBARCHIVE": lib_file})
-        if cls.libarchive:
-            reload(cls.libarchive)
-        else:
-            cls.libarchive = import_module("libarchive")
 
     def __init__(self, filename: str, use_zipfile=False) -> None:
         self.filename = filename
-        if not self.libarchive or use_zipfile:
+        if not os.path.isfile(os.environ.get("LIBARCHIVE")) or use_zipfile:
             logging.warning(
                 "using build-in ZipFile library on %s because libarchive is not available or user forcing" % filename)
             self.zf = ZipFile(self.filename)
@@ -37,6 +27,7 @@ class Decompress():
             self.extractFiles = extractFiles
             self.__del__ = self.zf.close
         elif filename.endswith(".exe"):
+            self.load_libarchive()
             logging.info("input an exe file, try extracting 7z sfx")
             self.reader = self.libarchive.memory_reader
             self.extracter = self.libarchive.extract_memory
@@ -52,9 +43,16 @@ class Decompress():
                     f.seek(-7, 1)
             self.read_from = f.read()
         else:
+            self.load_libarchive()
             self.reader = self.libarchive.file_reader
             self.extracter = self.libarchive.extract_file
             self.read_from = filename
+
+    def load_libarchive(self):
+        if self.libarchive:
+            reload(self.libarchive)
+        else:
+            self.libarchive=import_module("libarchive")
 
     def getFileList(self) -> Generator:
         with self.reader(self.read_from) as archive:
@@ -94,7 +92,7 @@ class Decompress():
         if not os.path.exists(outdir):
             os.makedirs(outdir)
         os.chdir(outdir)
-        logging.debug("chdir to %s, starting extraction"%outdir)
+        logging.debug("chdir to %s, starting extraction" % outdir)
         self.extracter(self.read_from)
         logging.debug("extraction complete")
         os.chdir(pwd_temp)
